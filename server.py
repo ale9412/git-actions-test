14c12f92ee2c342b54e81d59c877be0047ccaea4
alejandrodaniel.martin@georgfischer.com <alejandrodaniel.martin@georgfischer.com>
2023-05-28 13:24:44 +0200

    Fixed bytes output problem

--- server.py
+++ server.py
@@ -5,11 +5,14 @@ import os
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        self.wfile.write(b'Hello my f**king World')
-        self.wfile.write(b'Getting color %s from env'%os.getenv("COLOR"))
-
+        try:
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            self.wfile.write(b'Hello my f**king World\n')
+            string = 'Getting color %s from env'%os.getenv("COLOR")
+            self.wfile.write(bytes(string, "utf-8"))
+        except KeyboardInterrupt:
+            self.close()
 
 print("Serving on port 8000")
 httpd = socketserver.TCPServer(('', 8000), Handler)
