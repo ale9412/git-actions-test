e65900b16c7ca191972fca85f0cd2cb807f8aad5
alejandrodaniel.martin@georgfischer.com <alejandrodaniel.martin@georgfischer.com>
2023-05-28 12:34:06 +0200

    Print env variable in code

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import http.server
 import socketserver
 from http import HTTPStatus
-
+import os
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         self.wfile.write(b'Hello my f**king World')
+        self.wfile.write(b'Getting color %s from env'%os.getenv("COLOR"))
 
 
 print("Serving on port 8000")
